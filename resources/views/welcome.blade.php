3befb2b55831628ded71218f41496cde2e815344
biceps1 <kzkzk@hotmail.com>
2020-05-22 18:02:24 +0400

    aquamarine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #000;
+                background-color: #aquamarine;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;

4f77e2d811bced65e78ace12e5ddbbcb3f1bc843
biceps1 <kzkzk@hotmail.com>
2020-05-22 17:41:57 +0400

    red bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #000;
+                background-color: red;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
